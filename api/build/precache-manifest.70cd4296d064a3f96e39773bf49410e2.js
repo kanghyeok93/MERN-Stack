4806bcac9a284964c55f57b58fb92f78f9fb5c44
kanghyeok93 <kanghyeok93@naver.com>
2019-06-15 04:23:57 +0900

    modify file

---
+++ api/build/precache-manifest.70cd4296d064a3f96e39773bf49410e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4236b6fee211af40f27e1f96c2fdd7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50541296e9f35e60c4f6",
+    "url": "/static/css/main.b423a243.chunk.css"
+  },
+  {
+    "revision": "6de338295cf61fd4e184",
+    "url": "/static/js/2.bedcfecc.chunk.js"
+  },
+  {
+    "revision": "50541296e9f35e60c4f6",
+    "url": "/static/js/main.3b4f6e6b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- api/server.js
+++ api/server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('../build'));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname + '/app/api/build/index.html'))
+        res.sendFile(path.resolve(__dirname + '/build/index.html'))
     })
 }
 
