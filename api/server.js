0c437c70c4e353982dced1771873f5ddbfb02e16
kanghyeok93 <kanghyeok93@naver.com>
2019-06-15 03:40:27 +0900

    modify file

--- api/server.js
+++ api/server.js
@@ -44,7 +44,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('../build'));
 
     app.get('*', (req,res) => {
-        res.sendFile(path.resolve(__dirname + '/build/index.html')); // relative path
+        res.sendFile(path.join(__dirname,'build','index.html'));
     })
 }
 
